33d90ae56de1dd4cc4e7b369a67278f895e1707a
singhsuraj847 <kpt.singhsuraj1995@gmail.com>
2021-02-07 16:57:25 +0530

    second commit

--- bffbook/settings.py
+++ bffbook/settings.py
@@ -117,7 +117,6 @@ STATICFILES_DIRS = [
 STATIC_ROOT = os.path.join(os.path.dirname(
     BASE_DIR), "static_cdn", "static_root")
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(os.path.dirname(
